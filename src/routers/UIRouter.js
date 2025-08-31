960f0b035193b8804026501cc16fad76c980a25f
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2025-08-31 01:17:29 -0500

    routers modules updated

--- src/routers/UIRouter.js
+++ src/routers/UIRouter.js
@@ -3,10 +3,6 @@ import { Router } from "express";
 // Import the middleware to verify tokens from the authentication app
 import { authAppVerifyToken } from
 '../middlewares/tokenHandlers/authAppTokenHandler.js';
-// Import the middleware to verify the API key from the client app
-import { checkApiKey } from '../middlewares/apiAuthHandler.js';
-// Import the middleware to verify the data types sended in the request
-import { validatorHandler  } from '../middlewares/validatorHandler.js';
 // Import the controllers functions to manage user interfaces templates
 import { loginForm } from '../controllers/UI/loginForm.js';
 import { dashboard } from '../controllers/UI/dashboard.js';
@@ -19,11 +15,6 @@ export const UIRouter = Router();
 UIRouter.get(
   // Route path display the login view
   '/',
-  // Middleware to validate the data type
-  //validatorHandler(userSchema, 'body'),
-  // Middleware to verify the API key sended by the client before
-  // proceeding to the controller
-  //checkApiKey,
   // Controller function to render a login template
   loginForm
 );
@@ -32,11 +23,6 @@ UIRouter.get(
 UIRouter.get(
   // Route path display the dashboard view
   '/dashboard',
-  // Middleware to validate the data type
-  //validatorHandler(userSchema, 'body'),
-  // Middleware to verify the API key sended by the client before
-  // proceeding to the controller
-  //checkApiKey,
   // Middleware to verify the token before proceeding to the controller
   authAppVerifyToken,
   // Controller function to render a dashboard template
@@ -46,12 +32,7 @@ UIRouter.get(
 // Define a GET route for certificate academic form view
 UIRouter.get(
   // Route path display the certificate academic form view
-  '/certificates/academic',
-  // Middleware to validate the data type
-  //validatorHandler(userSchema, 'body'),
-  // Middleware to verify the API key sended by the client before
-  // proceeding to the controller
-  //checkApiKey,
+  '/profile',
   // Middleware to verify the token before proceeding to the controller
   authAppVerifyToken,
   // Controller function to render a dashboard template
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,12 +3,6 @@ import { Router } from "express";
 // Import the middleware to verify tokens from the authentication app
 import { authAppVerifyToken } from
 '../middlewares/tokenHandlers/authAppTokenHandler.js';
-// Import the middleware to verify the API key from the client app
-import { checkApiKey } from '../middlewares/apiAuthHandler.js';
-// Import the middleware to verify the data types sended in the request
-import { validatorHandler  } from '../middlewares/validatorHandler.js';
-// Import the user data types schema
-import { userSchema } from '../schemas/userSchema.js';
 // Import the controllers functions to authenticate and manage users
 import { loginUser } from '../controllers/users/login.js';
 import { createOneUser } from '../controllers/users/create.js';
@@ -24,11 +18,6 @@ export const userRouter = Router();
 userRouter.post(
   // Route path to authenticate a users
   '/login',
-  // Middleware to validate the data type
-  validatorHandler(userSchema, 'body'),
-  // Middleware to verify the API key sended by the client before
-  // proceeding to the controller
-  //checkApiKey,
   // Controller function to login a user
   loginUser
 );
@@ -37,11 +26,6 @@ userRouter.post(
 userRouter.post(
   // Route path to create a user
   '/create',
-  // Middleware to validate the data type
-  validatorHandler(userSchema, 'body'),
-  // Middleware to verify the API key sended by the client before
-  // proceeding to the controller
-  //checkApiKey,
   // Middleware to verify the token before proceeding to the controller
   authAppVerifyToken,
   // Controller function to create the user
@@ -52,11 +36,6 @@ userRouter.post(
 userRouter.post(
   // Route path to update a user
   '/update',
-  // Middleware to validate the data type
-  validatorHandler(userSchema, 'body'),
-  // Middleware to verify the API key sended by the client before
-  // proceeding to the controller
-  //checkApiKey,
   // Middleware to verify the token before proceeding to the controller
   authAppVerifyToken,
   // Controller function to update the reader
@@ -67,11 +46,6 @@ userRouter.post(
 userRouter.post(
   // Route path to delete a user
   '/delete',
-  // Middleware to validate the data type
-  validatorHandler(userSchema, 'body'),
-  // Middleware to verify the API key sended by the client before
-  // proceeding to the controller
-  //checkApiKey,
   // Middleware to verify the token before proceeding to the controller
   authAppVerifyToken,
   // Controller function to delete the user
@@ -82,11 +56,6 @@ userRouter.post(
 userRouter.post(
   // Route path to list an user
   '/listone',
-  // Middleware to validate the data type
-  validatorHandler(userSchema, 'body'),
-  // Middleware to verify the API key sended by the client before
-  // proceeding to the controller
-  //checkApiKey,
   // Middleware to verify the token before proceeding to the controller
   authAppVerifyToken,
   // Controller function to list a user
@@ -97,11 +66,6 @@ userRouter.post(
 userRouter.get(
   // Route path to list all users
   '/listall',
-  // Middleware to validate the data type
-  //validatorHandler(userSchema, 'body'),
-  // Middleware to verify the API key sended by the client before
-  // proceeding to the controller
-  //checkApiKey,
   // Middleware to verify the token before proceeding to the controller
   authAppVerifyToken,
   // Controller function to list all users
